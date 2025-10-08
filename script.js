dd026574c96263ab98063a15f80c574f8c37beb4
NokuphilaMpila <136009909+NokuphilaMpila@users.noreply.github.com>
2025-10-08 15:56:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,94 @@
+// Science Quiz Questions and Answers
+<script>
+    const questions = [
+        {
+            question: "How many elements are on the periodic table?"
+            answers: [
+                {text: "92", correct:false},
+                {text: "118", correect:true},
+                {text: "138", correct:false},
+                {text: "76", correct:false}
+            ]
+        },
+        {
+            question: "What element is the most commonly used to create nuclear energy?"
+            answers: [
+                {text: "Hydrogen", correct:false},
+                {text: "Lithium", correct:false},
+                {text: "Uranium", correct:true},
+                {text: "Sodium", correct:false}
+            ]
+        },
+        {
+            question: "Whatr is the symbol for iron on the periodic table?"
+            answers: [
+                {text: "He", correct:false},
+                {text: "Ne", correct:false},
+                {text: "O", correct:false},
+                {text: "Fe", correct: true}
+            ]
+        }
+    ]
+
+// Mathematics Questions and Answers
+ const questions = [
+    {
+        question: "What is the sum of 555 + 555 + 123?"
+        answers: [
+            {text: "1244", correct:false},
+            {text: "1233", correct:true},
+            {text: "1103", correct:false},
+            {text: "1234", correct:false}
+        ]
+    },
+    {
+        question: "What is 30 x 18?"
+        answers: [
+            {text:"230", correct:false};
+            {text:"380", correct:false},
+            {text:"540", correct:true},
+            {text:"740", correct:false}
+        ]
+    },
+    {
+        question: "What is 700 divided by 4?"
+        answers: [
+            {text: "200", correct:false},
+            {text:"175", correct:true},
+            {text: "181", correct:false},
+            {text:"70", correct:false}
+        ]
+    }
+ ]
+
+ //Movies Questions and Answers
+ const= questions [
+    {
+        question: "What colors make up Freddy Kruegers stripped sweater?"
+        answers: [
+            {text: "Blue and Red", correct:false},
+            {text: "Yellow and Green", correct:false},
+            {text: "Red and Green", correct:true},
+            {text: "Black and Red", correct:false}
+        ]
+    },
+    {
+        question: "Who directed the hit 2017 movie Get Out?"
+        answers: [
+            {text: "Steven Spielberg", correct:false},
+            {text:"Jordan Peele", correct:true},
+            {text: "Tim Burton", correct:false},
+            {text:"Spike Lee", correct:false}
+        ]
+    },
+    {
+        question: "If you watch the Marvel Movies in chronological order, which would you watch first?"
+        answers: [
+            {text: "Captain America: The First Avenger",correct:true},
+            {text: "Captain America: The Winter Soldier", correct:false},
+            {text: "Avengers", correct:false}
+            {text:"Captain Marvel", correct:false}
+        ]
+    }
+ ]
+</script>
